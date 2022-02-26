559f8427be5bf5b5aed9ca637e5a23c8aa0d2dd6
Georgia Hunt <georgialhunt89@gmail.com>
2022-02-26 22:17:35 +1030

    Added input binding for cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular_Cards';
+posts = [
+  {
+    title: 'Neat Tree',
+    imageUrl: 'assets/tree.jpeg',
+    username: 'nature',
+    content: 'I saw this amazing tree today'
+  },
+  {
+    title: 'Snowy Mountain',
+    imageUrl: 'assets/mountain.jpeg',
+    username: 'mountainlover',
+    content: 'Here is my favourite mountain'
+  },
+  {
+    title: 'Mountain Biking',
+    imageUrl: 'assets/biking.jpeg',
+    username: 'Biking111',
+    content: 'Picture from some mountain biking last weekend'
+  },
+];
+
 }
